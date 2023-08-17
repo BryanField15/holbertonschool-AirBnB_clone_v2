abff2a646d8ab4771a753815e2ce7448891cafe3
BryanField15 <6367@holbertonstudents.com>
2023-08-17 16:49:12 -0700

    task 7 solution

--- models/state.py
+++ models/state.py
@@ -4,23 +4,27 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """Class definition for the state class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City", back_populates="state", cascade="all, delete, delete-orphan")
 
-    @property
-    def cities(self):
-        """returns the list of City instance that belong to that State"""
-        from models import storage
-        from models.city import City
-        all_cities_dict = storage.all(City)
-        return_list = []
-        for key, value in all_cities_dict.items():
-            if self.id == value.state_id:
-                return_list.append(value)
-        return return_list
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities_relation = relationship(
+            "City", back_populates="state", cascade="all, delete, delete-orphan"
+        )
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instance that belong to that State"""
+            from models import storage
+            from models.city import City
+            all_cities_dict = storage.all(City)
+            return_list = []
+            for key, value in all_cities_dict.items():
+                if self.id == value.state_id:
+                    return_list.append(value)
+                    return return_list
