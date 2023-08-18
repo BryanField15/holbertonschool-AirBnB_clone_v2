87783c8bfb421a0531f4773d8257802bf816fea3
BryanField15 <6367@holbertonstudents.com>
2023-08-17 21:55:10 -0700

    task 7 solution

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,9 +12,8 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """html page with a list of all state objects sorted az"""
-    states = storage.all(State)
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template("7-states_list.html", states=sorted_states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
