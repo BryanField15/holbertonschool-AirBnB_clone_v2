38e95f1b076c1b8bac36f9be58c73a6b3287423a
BryanField15 <6367@holbertonstudents.com>
2023-08-17 23:31:11 -0700

    updated order of file storage

--- models/state.py
+++ models/state.py
@@ -12,20 +12,20 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities_relation = relationship(
-            "City", back_populates="state",
-            cascade="all, delete, delete-orphan"
-        )
-    else:
-        @property
-        def cities(self):
-            """returns the list of City instance that belong to that State"""
-            from models import storage
-            from models.city import City
-            all_cities_dict = storage.all(City)
-            return_list = []
-            for key, value in all_cities_dict.items():
-                if self.id == value.state_id:
-                    return_list.append(value)
-            return return_list
+if getenv("HBNB_TYPE_STORAGE") != "db":
+    @property
+    def cities(self):
+        """returns the list of City instance that belong to that State"""
+        from models import storage
+        from models.city import City
+        all_cities_dict = storage.all(City)
+        return_list = []
+        for key, value in all_cities_dict.items():
+            if self.id == value.state_id:
+                return_list.append(value)
+        return return_list
+else:
+    cities = relationship(
+        "City",
+        back_populates="state",
+        cascade="all, delete, delete-orphan")
